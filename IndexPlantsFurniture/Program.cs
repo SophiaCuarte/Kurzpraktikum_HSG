cd62d3f0ee0209c70fb5e1ca83833606a5b1397a
Sophia Cuarte <Sophia.Cuarte@ksb-sg.ch>
2023-09-21 12:50:24 +0200

    Index with plants done

---
+++ IndexPlantsFurniture/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace IndexPlantsFurniture
+{
+    public class Example
+    {
+        public static void Main()
+        {
+            List<string> plants = new List<string>() { "Blume", "Blatt", "Stiel", "Bluete", "Gruen" };
+            List<string> plants2 = new List<string>() { "Baum", "Stamm", "Ast", "Bluete", "Gruen", "Blatt" };
+            List<string> plants3 = new List<string>() { "Pilz", "Hut", "Farbe", "Stiel" };
+
+            Dictionary<string, List<string>> plantsnew = new Dictionary<string, List<string>>();
+
+            if (plants.Count > 0)
+            {
+                string key = plants[0];
+                foreach (string value in plants.Skip(1))
+                {
+                    if (!plantsnew.ContainsKey(value))
+                    {
+                        plantsnew[value] = new List<string>();
+                    }
+                    plantsnew[value].Add(key);
+                }
+            }
+
+            if (plants2.Count > 0)
+            {
+                string key = plants2[0];
+                foreach (string value in plants2.Skip(1))
+                {
+                    if (!plantsnew.ContainsKey(value))
+                    {
+                        plantsnew[value] = new List<string>();
+                    }
+                    plantsnew[value].Add(key);
+                }
+            }
+
+            if (plants3.Count > 0)
+            {
+                string key = plants3[0];
+                foreach (string value in plants3.Skip(1))
+                {
+                    if (!plantsnew.ContainsKey(value))
+                    {
+                        plantsnew[value] = new List<string>();
+                    }
+                    plantsnew[value].Add(key);
+                }
+            }
+
+            foreach (var kvp in plantsnew)
+            {
+                Console.WriteLine($"{kvp.Key}: {string.Join(", ", kvp.Value)}");
+            }
+        }
+    }
+}
--- IndexTest/Program.cs
+++ IndexTest/Program.cs
@@ -1,19 +1,27 @@
-﻿namespace IndexTryout
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Dictionary<string, string> Plants = new Dictionary<string, string>();
+﻿
 
-            Plants.Add("Blatt", "Blume, Baum");
-            Plants.Add("Stiel", "Blume, Pilz");
+var dicPlant = new Dictionary<string, List<string>>() {
+    {"Blume", new List<string> { "Blatt", "Stiel", "Blüte", "Grün" } },
+    {"Baum", new List<string> { "Blatt", "Ast", "Blüte", "Grün", "Stamm" } },
+    {"Pilz", new List<string> { "Hut", "Farbe", "Stiel" } }
+};
 
-            foreach (KeyValuePair<string, string> feature in Plants)
-            {
-                Console.WriteLine( feature.Key + ": " + feature.Value);
-            }
+var dicFurniture = new Dictionary<string, List<string>>() {
+    {"Tisch", new List<string> { "Holz", "Beine" } },
+    {"Schrank", new List<string> { "Holz", "Tür" } },
+    {"Stuhl", new List<string> { "Lehne", "Beine", "Sitzfläche" } },
+    {"Bett", new List<string> { "Holz", "Beine", "Laken" } }
+};
 
-        }
-    }
-}
+Dictionary<string, List<string>> Index(Dictionary<string, List<string>> defLst) =>
+    defLst.SelectMany(features => features.Value).GroupBy(features => features).Select(features => features.Key)
+    .Select(u => new {
+        unique = u,
+        lst = defLst.Where(d => d.Value.Contains(u))
+    .Select(features => features.Key).ToList()
+    }).ToDictionary(features => features.unique, features => features.lst);
+
+void Print(Dictionary<string, List<string>> dic) => Index(dic).ToList().ForEach(x => Console.WriteLine($"{x.Key} - {string.Join(", ", x.Value)}"));
+
+Print(dicPlant);
+Print(dicFurniture);
