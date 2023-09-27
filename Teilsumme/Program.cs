43aeb1feaa0e293811694f0f8c25fc549520e5ea
Sophia Cuarte <Sophia.Cuarte@ksb-sg.ch>
2023-09-27 13:10:04 +0200

    Teilsumme fertig

--- Teilsumme/Program.cs
+++ Teilsumme/Program.cs
@@ -1,38 +1,46 @@
 ﻿using System;
 
-namespace StockAnalysis
+namespace Teilsumme
 {
     class Program
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine("Geben Sie die Anfangszahl ein:");
+            var rates = new double[] { 127.5, -0.5, 2, -1, 1, 3.5, -13, 7, -2, -6, -9, -21, -17, -5, 0.5, 4, -7, -12, 2.5, -3, 2 };
+            double[] results = new double[rates.Length];
+            
+            results[0] = rates[0];
 
-            //Console.WriteLine("Wie viele Änderungen möchten Sie eingeben?");
-            //int length = Convert.ToInt32(Console.ReadLine());
-
-            var rates = new  double [] { 127.5, -0.5, 2, - 1, 1, 3.5, -13, 7, -2, -6, -9, -21, -17, -5, 0.5, 4, -7, -12, 2.5, -3, 2};
-
-            double maxReturn = double.MinValue;
-            double minRate = double.MinValue;
-            int buyDay;
-            int sellDay;
-
-
-            for(var i = 0; i < rates.Length; i++)
+            for (int i = 1; i < rates.Length; i++)
             {
-
-                if (i < rates.Length-1)
-                {
-                    minRate = rates[i] + rates[i + 1];
-                    maxReturn = rates[i] - minRate;
-                        }
-                Console.WriteLine($"day: {i} - minRate {minRate} - maxReturn: {maxReturn}");
-
+                results[i] = results[i - 1] + rates[i];
             }
 
-            Console.ReadLine();
+            double maxPercentProfit = double.MinValue;
+            int buyDay = 0;
+            int sellDay = 0;
+            double percentProfit = double.MinValue;
+            
 
+            for (int buy = 1; buy < results.Length; buy++)
+            {
+                for (int sell = buy + 1; sell < results.Length; sell++)
+                {
+                    double diff = results[sell - 1] - results[buy - 1];
+                    percentProfit = (diff / results[buy]) * 100;
+
+                    if (percentProfit > maxPercentProfit)
+                    {
+                        maxPercentProfit = percentProfit;
+                        buyDay = buy;
+                        sellDay = sell;
+                    }
+                }
+            }
+
+            Console.WriteLine($"Ein bester Einkaufstag wäre der {buyDay}. Börsentag gewesen, ein dazugehöriger Verkaufstag der {sellDay}. \nDer so realisierbare Gewinn wäre {maxPercentProfit} % vom eingesetzten Betrag gewesen.");
+            Console.WriteLine($"Tag {buyDay} = {results[buyDay - 1]}");
+            Console.WriteLine($"Tag {sellDay} = {results[sellDay - 1]}");
         }
     }
 }
---
+++ TeilsummeTest/UnitTest1.cs
@@ -0,0 +1,24 @@
+//using Microsoft.VisualStudio.TestPlatform.TestHost;
+//using Microsoft.VisualStudio.TestTools.UnitTesting;
+//using Teilsumme;
+
+//namespace TeilsummeTests
+//{
+//    [TestClass]
+//    public class ProgramTests
+//    {
+//        [TestMethod]
+//        public void TestBestBuyAndSellDays()
+//        {
+//            decimal[] priceChanges = { 127.5m, -0.5m, 2m, -1m, 1m, 3.5m, -13m, 7m, -2m, -6m, -9m, -21m, -17m, -5m, 0.5m, 4m, -7m, -12m, 2.5m, -3m, 2m };
+
+//            var result = Program.
+
+//            var expectedBuyDay = 7;
+//            var expectedSellDay = 8;
+//            Assert.AreEqual(expectedBuyDay, result.BuyDay);
+//            Assert.AreEqual(expectedSellDay, result.SellDay);
+//        }
+//    }
+//    }
+//}
